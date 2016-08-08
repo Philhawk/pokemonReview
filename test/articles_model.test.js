3a353bd033a4901ec318a0bdaaa4634b18bbcc68
Gabriel Lebec <glebec@gmail.com>
2016-08-08 02:22:18 -0400

    formatting

--- test/articles_model.test.js
+++ test/articles_model.test.js
@@ -30,10 +30,12 @@ describe('Articles', function () {
    * http://docs.sequelizejs.com/en/stable/docs/models-definition/#validations
    */
   it('has title and content fields', function () {
+
     return Article.create({
       title: 'Migratory Birds',
       content: fullText
-    }).then(function (savedArticle) {
+    })
+    .then(function (savedArticle) {
       expect(savedArticle.title).to.equal('Migratory Birds');
       expect(savedArticle.content).to.equal(fullText);
     });
@@ -47,11 +49,10 @@ describe('Articles', function () {
     });
 
     return article.validate()
-      .then(function(result) {
-        expect(result).to.be.an.instanceOf(Error);
-        expect(result.message).to.contain('content cannot be null');
-      });
-
+    .then(function(result) {
+      expect(result).to.be.an.instanceOf(Error);
+      expect(result.message).to.contain('content cannot be null');
+    });
 
   });
 
@@ -63,25 +64,28 @@ describe('Articles', function () {
     });
 
     return article.validate()
-      .then(function (result) {
-        expect(result).to.be.an.instanceOf(Error);
-        expect(result.message).to.contain('Validation error');
-      });
+    .then(function (result) {
+      expect(result).to.be.an.instanceOf(Error);
+      expect(result.message).to.contain('Validation error');
+    });
 
   });
 
   xit('can handle long content', function() {
+
     var articleContent = 'WALL-E (stylized with an interpunct as WALL·E) is a 2008 American computer-animated science-fiction comedy film produced by Pixar Animation Studios and released by Walt Disney Pictures. Directed by Andrew Stanton, the story follows a robot named WALL-E, who is designed to clean up an abandoned, waste-covered Earth far in the future. He falls in love with another robot named EVE, who also has a programmed task, and follows her into outer space on an adventure that changes the destiny of both his kind and humanity. Both robots exhibit an appearance of free will and emotions similar to humans, which develop further as the film progresses.';
+
     return Article.create({
       title: 'WALL-E',
       content: articleContent
-    }).then(function(result) {
+    })
+    .then(function(result) {
       expect(result).to.be.an('object');
       expect(result.title).to.equal('WALL-E');
       expect(result.content).to.equal(articleContent);
     });
-  });
 
+  });
 
 
   /**
@@ -92,7 +96,8 @@ describe('Articles', function () {
    */
   xit('has a virtual 23-character snippet field appended with "..."', function () {
 
-    return Article.findOne({ where: { title: 'Migratory Birds' } }).then(function (article) {
+    return Article.findOne({ where: { title: 'Migratory Birds' } })
+    .then(function (article) {
       expect(article.content).to.equal(fullText);
       expect(article.snippet).to.equal('The South African cliff...');
     });
@@ -109,7 +114,8 @@ describe('Articles', function () {
    */
   xit('has an instance method to truncate the content', function () {
 
-    return Article.findOne({ where: { title: 'Migratory Birds' } }).then(function (article) {
+    return Article.findOne({ where: { title: 'Migratory Birds' } })
+    .then(function (article) {
       expect(article.content).to.equal(fullText);
       article.truncate(12);
       expect(article.content).to.equal('The South Af');
@@ -119,7 +125,8 @@ describe('Articles', function () {
 
   xit('content truncation accepts any length', function () {
 
-    return Article.findOne({ where: { title: 'Migratory Birds' } }).then(function (article) {
+    return Article.findOne({ where: { title: 'Migratory Birds' } })
+    .then(function (article) {
       var randLength = Math.ceil(Math.random() * 20);
       expect(article.content).to.equal(fullText);
       article.truncate(randLength);
@@ -129,7 +136,9 @@ describe('Articles', function () {
   });
 
   xit('does not save the content once truncated', function() {
-    return Article.findOne({ where: { title: 'Migratory Birds' } }).then(function(article) {
+
+    return Article.findOne({ where: { title: 'Migratory Birds' } })
+    .then(function(article) {
       expect(article.content).to.equal(fullText);
       article.truncate(7);
       expect(article.content).to.have.length(7);
@@ -138,6 +147,7 @@ describe('Articles', function () {
     .then(function(article) {
       expect(article.content).to.equal(fullText);
     });
+
   });
 
   /**
@@ -150,10 +160,10 @@ describe('Articles', function () {
   xit('class method findByTitle finds one article by its title', function () {
 
     return Article.findByTitle('Migratory Birds')
-      .then(function (article) {
-        expect(article).not.to.be.an.instanceOf(Array);
-        expect(article.content).to.equal(fullText);
-      });
+    .then(function (article) {
+      expect(article).not.to.be.an.instanceOf(Array);
+      expect(article.content).to.equal(fullText);
+    });
 
   });
 
@@ -165,28 +175,30 @@ describe('Articles', function () {
    */
 
   xit('belongs to a user, who is stored as the article\'s `author`', function() {
+
     var user;
     return User.create({ name: 'Alatar the Blue'})
-      .then(function(u) {
-        user = u;
-        return Article.create({
-          title: 'Blue Wizards',
-          content: 'They are two of the five Wizards (or Istari) sent by the Valar to Middle-earth to aid in the struggle against Sauron.'
-        });
-      })
-      .then(function(article) {
-        return article.setAuthor(user);
-      })
-      .then(function() {
-        return Article.findOne({
-          where: { title: 'Blue Wizards' },
-          include: { model: User, as: 'author' }
-        });
-      })
-      .then(function(article){
-        expect(article.author).to.exist;
-        expect(article.author.name).to.equal('Alatar the Blue');
+    .then(function(createdUser) {
+      user = createdUser;
+      return Article.create({
+        title: 'Blue Wizards',
+        content: 'They are two of the five Wizards (or Istari) sent by the Valar to Middle-earth to aid in the struggle against Sauron.'
       });
+    })
+    .then(function(article) {
+      return article.setAuthor(user);
+    })
+    .then(function() {
+      return Article.findOne({
+        where: { title: 'Blue Wizards' },
+        include: { model: User, as: 'author' }
+      });
+    })
+    .then(function(article){
+      expect(article.author).to.exist; // eslint-disable-line no-unused-expressions
+      expect(article.author.name).to.equal('Alatar the Blue');
+    });
+
   });
 
   /**
@@ -197,6 +209,7 @@ describe('Articles', function () {
    */
 
   describe('has a version field', function() {
+
     before(function() {
       return Article.create({
         title: 'Biological Immortality',
@@ -205,13 +218,16 @@ describe('Articles', function () {
     });
 
     xit('that is originally 0', function() {
+
       return Article.findOne({where: {title: 'Biological Immortality'}})
-        .then(function(article) {
-          expect(article.version).to.equal(0);
-        });
+      .then(function(article) {
+        expect(article.version).to.equal(0);
+      });
+
     });
 
     xit('that increments by 1 every time you update the document', function() {
+
       return Article.findOne({where: {title: 'Biological Immortality'}})
       .then(function(article) {
         article.content = 'Biological immortality is a lie!';
@@ -220,7 +236,9 @@ describe('Articles', function () {
       .then(function(article) {
         expect(article.version).to.equal(1);
       });
+
     });
+
   });
 
   /** EXTRA CREDIT
@@ -233,6 +251,7 @@ describe('Articles', function () {
    * To activate this spec, change `xit` to `it`
    */
   xit('has a `tags` field with a custom getter', function () {
+
     return Article.create({
       title: 'Taggy',
       content: 'So Taggy',
@@ -241,6 +260,7 @@ describe('Articles', function () {
     .then(function (article) {
       expect(article.tags).to.equal('tag1, tag2, tag3');
     });
+
   });
 
 });
--- test/articles_route.test.js
+++ test/articles_route.test.js
@@ -38,14 +38,14 @@ describe('Articles Route:', function () {
     xit('responds with an array via JSON', function () {
 
       return agent
-        .get('/articles')
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .expect(function (res) {
-          // res.body is the JSON return object
-          expect(res.body).to.be.an.instanceOf(Array);
-          expect(res.body).to.have.length(0);
-        });
+      .get('/articles')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .expect(function (res) {
+        // res.body is the JSON return object
+        expect(res.body).to.be.an.instanceOf(Array);
+        expect(res.body).to.have.length(0);
+      });
 
     });
 
@@ -65,12 +65,12 @@ describe('Articles Route:', function () {
       return article.save().then(function () {
 
         return agent
-          .get('/articles')
-          .expect(200)
-          .expect(function (res) {
-            expect(res.body).to.be.an.instanceOf(Array);
-            expect(res.body[0].content).to.equal('Test body');
-          });
+        .get('/articles')
+        .expect(200)
+        .expect(function (res) {
+          expect(res.body).to.be.an.instanceOf(Array);
+          expect(res.body[0].content).to.equal('Test body');
+        });
 
       });
 
@@ -92,13 +92,13 @@ describe('Articles Route:', function () {
       return article.save().then(function () {
 
         return agent
-          .get('/articles')
-          .expect(200)
-          .expect(function (res) {
-            expect(res.body).to.be.an.instanceOf(Array);
-            expect(res.body[0].content).to.equal('Test body');
-            expect(res.body[1].content).to.equal('Another test body');
-          });
+        .get('/articles')
+        .expect(200)
+        .expect(function (res) {
+          expect(res.body).to.be.an.instanceOf(Array);
+          expect(res.body[0].content).to.equal('Test body');
+          expect(res.body[1].content).to.equal('Another test body');
+        });
 
       });
 
@@ -130,24 +130,28 @@ describe('Articles Route:', function () {
      * where we search by the ID of the article created above
      */
     xit('returns the JSON of the article based on the id', function () {
+
       return agent
-        .get('/articles/' + article.id)
-        .expect(200)
-        .expect(function (res) {
-          if (typeof res.body === 'string') {
-            res.body = JSON.parse(res.body);
-          }
-          expect(res.body.title).to.equal('Second Article');
-        });
+      .get('/articles/' + article.id)
+      .expect(200)
+      .expect(function (res) {
+        if (typeof res.body === 'string') {
+          res.body = JSON.parse(res.body);
+        }
+        expect(res.body.title).to.equal('Second Article');
+      });
+
     });
 
     /**
      * Here we pass in a bad ID to the URL, we should get a 404 error
      */
     xit('returns a 404 error if the ID is not correct', function () {
+
       return agent
-        .get('/articles/' + '74')
-        .expect(404);
+      .get('/articles/74')
+      .expect(404);
+
     });
 
   });
@@ -173,28 +177,32 @@ describe('Articles Route:', function () {
      * }
      */
     xit('creates a new article', function () {
+
       return agent
-        .post('/articles')
-        .send({
-          title: 'Awesome POST-Created Article',
-          content: 'Can you believe I did this in a test?'
-        })
-        .expect(200)
-        .expect(function (res) {
-          expect(res.body.message).to.equal('Created successfully');
-          expect(res.body.article.id).to.not.be.an('undefined');
-          expect(res.body.article.title).to.equal('Awesome POST-Created Article');
-        });
+      .post('/articles')
+      .send({
+        title: 'Awesome POST-Created Article',
+        content: 'Can you believe I did this in a test?'
+      })
+      .expect(200)
+      .expect(function (res) {
+        expect(res.body.message).to.equal('Created successfully');
+        expect(res.body.article.id).to.not.be.an('undefined');
+        expect(res.body.article.title).to.equal('Awesome POST-Created Article');
+      });
+
     });
 
     // This one should fail with a 500 because we don't set the article.body
     xit('does not create a new article without a body', function () {
+
       return agent
-        .post('/articles')
-        .send({
-          title: 'This Article Should Not Be Allowed'
-        })
-        .expect(500);
+      .post('/articles')
+      .send({
+        title: 'This Article Should Not Be Allowed'
+      })
+      .expect(500);
+
     });
 
     // Check if the articles were actually saved to the database
@@ -204,8 +212,9 @@ describe('Articles Route:', function () {
         where: {
           title: 'Awesome POST-Created Article'
         }
-      }).then(function (article) {
-        expect(article).to.exist;
+      })
+      .then(function (article) {
+        expect(article).to.exist; // eslint-disable-line no-unused-expressions
         expect(article.content).to.equal('Can you believe I did this in a test?');
       });
 
@@ -227,9 +236,12 @@ describe('Articles Route:', function () {
         where: {
           title: 'Awesome POST-Created Article'
         }
-      }).then(function (_article) {
+      })
+      .then(function (_article) {
         article = _article;
-      }).catch(function(e) { console.error(e.message); });
+      })
+      .catch(function(err) { console.error(err.message); });
+
     });
 
     /**
@@ -249,34 +261,36 @@ describe('Articles Route:', function () {
     xit('updates an article', function () {
 
       return agent
-        .put('/articles/' + article.id)
-        .send({
-          title: 'Awesome PUT-Updated Article'
-        })
-        .expect(200)
-        .expect(function (res) {
-          expect(res.body.message).to.equal('Updated successfully');
-          expect(res.body.article.id).to.not.be.an('undefined');
-          expect(res.body.article.title).to.equal('Awesome PUT-Updated Article');
-          expect(res.body.article.content).to.equal('Can you believe I did this in a test?');
-        });
+      .put('/articles/' + article.id)
+      .send({
+        title: 'Awesome PUT-Updated Article'
+      })
+      .expect(200)
+      .expect(function (res) {
+        expect(res.body.message).to.equal('Updated successfully');
+        expect(res.body.article.id).to.not.be.an('undefined');
+        expect(res.body.article.title).to.equal('Awesome PUT-Updated Article');
+        expect(res.body.article.content).to.equal('Can you believe I did this in a test?');
+      });
 
     });
 
     xit('saves updates to the DB', function () {
 
-      return Article.findById(article.id).then(function (article) {
-        expect(article).to.exist;
-        expect(article.title).to.equal('Awesome PUT-Updated Article');
+      return Article.findById(article.id).then(function (foundArticle) {
+        expect(foundArticle).to.exist; // eslint-disable-line no-unused-expressions
+        expect(foundArticle.title).to.equal('Awesome PUT-Updated Article');
       });
 
     });
 
     xit('gets 500 for invalid update', function () {
+
       return agent
-        .put('/articles/' + article.id)
-        .send({ title: '' })
-        .expect(500);
+      .put('/articles/' + article.id)
+      .send({ title: '' })
+      .expect(500);
+
     });
 
   });
